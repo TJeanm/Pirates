de012a0ab3914c27922259bc6c78203eb9e2b837
TJeanm <theo.jeanmart@univ-tlse3.fr>
2025-02-13 11:59:17 +0100

    pirates

---
+++ src/affichage/Affichage.java
@@ -0,0 +1,19 @@
+package affichage;
+import java.util.Scanner;
+
+
+public class Affichage {
+	private Scanner sca=new Scanner(System.in);
+	public void afficherBienvenue() {
+		System.out.println("Bienvenue dans le jeu des pirates");
+	}
+	
+	public String recupererNomJoueur(int numeroJoueur) {
+		System.out.println("Entrez le nom du joueur "+numeroJoueur+" : ");
+		return sca.next();
+	}
+	
+	void main() {
+		
+	}
+}
