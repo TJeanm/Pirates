cb64f32b181f3830a3c601e196a49f25750f81cf
TJeanm <theo.jeanmart@univ-tlse3.fr>
2025-02-20 12:01:39 +0100

    affichage + classe cartes

---
+++ src/jeu/Carte.java
@@ -0,0 +1,46 @@
+package jeu;
+
+//xixhoj-3Botsi-xekjeg
+public class Carte {
+	private String nom;
+	private String description;
+	private int modif_popularite;
+	private int modif_vie;
+	
+	public Carte(String nom,String description, int modif_popularite, int modif_vie) {
+		this.nom=nom;
+		this.description=description;
+		this.modif_popularite=modif_popularite;
+		this.modif_vie=modif_vie;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getModif_popularite() {
+		return modif_popularite;
+	}
+
+	public void setModif_popularite(int modif_popularite) {
+		this.modif_popularite = modif_popularite;
+	}
+
+	public int getModif_vie() {
+		return modif_vie;
+	}
+
+	public void setModif_vie(int modif_vie) {
+		this.modif_vie = modif_vie;
+	}
+	
+	
+}
--- src/jeu/Joueur.java
+++ src/jeu/Joueur.java
@@ -1,6 +1,33 @@
 package jeu;
 
 public class Joueur {
-	private int popularite;
-	private int pv;
+	private String nom;
+	private int popularite=0;
+	private int vie=5;
+	
+	public Joueur(String nom) {
+		this.nom=nom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public int getPopularite() {
+		return popularite;
+	}
+
+	public void setPopularite(int popularite) {
+		this.popularite = popularite;
+	}
+
+	public int getVie() {
+		return vie;
+	}
+
+	public void setVie(int vie) {
+		this.vie = vie;
+	}
+	
+	
 }
